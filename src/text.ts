db85c4976731f44aedc22e3c8d04486e2aff8293
WAKAYAMA Shirou <shirou.faw@gmail.com>
2017-01-12 14:09:44 +0900

    use revealLine to recenter.

--- src/text.ts
+++ src/text.ts
@@ -24,38 +24,12 @@ export function activateText(context: vscode.ExtensionContext) {
 		scrollPosition = Scroll.Center;
 	});
     context.subscriptions.push(vscode.commands.registerCommand(ExtPrefix + ".recenter", () => {
-		if (scrollPosition === Scroll.Center) {
-			vscode.window.activeTextEditor.revealRange(
-				vscode.window.activeTextEditor.selection,
-				vscode.TextEditorRevealType.InCenter
-			);
-			scrollPosition = Scroll.Top;
-		} else if (scrollPosition === Scroll.Top) {
-			let promises = [
-				vscode.commands.executeCommand("scrollPageDown"),
-				vscode.commands.executeCommand("scrollPageDown")
-			];
-
-			Promise.all(promises).then(() => {
-				vscode.window.activeTextEditor.revealRange(
-					vscode.window.activeTextEditor.selection,
-					vscode.TextEditorRevealType.Default
-				);
-				scrollPosition = Scroll.Bottom;
-			});
-		} else if (scrollPosition === Scroll.Bottom) {
-			let promises = [
-				vscode.commands.executeCommand("scrollPageUp"),
-				vscode.commands.executeCommand("scrollPageUp")
-			];
-
-			Promise.all(promises).then(() => {
-				vscode.window.activeTextEditor.revealRange(
-					vscode.window.activeTextEditor.selection,
-					vscode.TextEditorRevealType.Default
-				);
-				scrollPosition = Scroll.Center;
-			});
-		}
+		const editor = vscode.window.activeTextEditor;
+        const pos = editor.selection.active;
+        const line = editor.document.lineAt(pos.line);
+        vscode.commands.executeCommand('revealLine', {
+			'lineNumber': line.lineNumber,
+            'at': 'center',
+        });
 	}));
 }
